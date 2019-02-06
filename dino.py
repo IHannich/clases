0641a7d438b634fc882bc69555749a2000215377
Ingrid <ingrid_torales@hotmail.com>
2019-02-06 16:43:10 -0300

    Se agrego persona.py al repositorio

---
+++ dino.py
@@ -0,0 +1,28 @@
+class Dino:
+    #cola, dientes, color, size
+    #def __init__(self, nombre):                   #init: crea, van los argumentos
+       # print("Hola soy un dinosaurio, me llamo", nombre)
+
+#pepito = Dino("pepe")
+
+#agregarle una propiedad color a la clase Dino, y que salude
+#diciendo de que color es el dinosaurio
+
+    #def __init__(self, nombre, color):
+     #   print("Hola soy un dinosaurio, me llamo", nombre, "y soy de color", color)
+
+#pepito = Dino("pepe", "verde")
+
+#agregarle una propiedad color a la clase Dino, y que salude
+#diciendo su nombre y de que color es el dinosaurio
+Class Dino: 
+    def __init__(self, un_nombre="", color="verde"):
+        self.nombre = un_nombre
+        self.color = un_color
+        print("Hola soy un dinosaurio, me llamo", self.nombre, "y soy de color", self.color)
+
+pepito = Dino("pepe", "verde")
+pepita = Dino("pepa", "azul")
+
+#agregarle una propiedad color a la clase Dino, y que salude
+#diciendo su nombre y de que color es el dinosaurio
