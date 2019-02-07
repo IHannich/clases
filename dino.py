217a9eb02bb82092ec1775bfee487d1e904b623f
Ingrid <ingrid_torales@hotmail.com>
2019-02-07 11:49:44 -0300

    no agregue nada

--- dino.py
+++ dino.py
@@ -15,14 +15,21 @@ class Dino:
 
 #agregarle una propiedad color a la clase Dino, y que salude
 #diciendo su nombre y de que color es el dinosaurio
-Class Dino: 
-    def __init__(self, un_nombre="", color="verde"):
-        self.nombre = un_nombre
+class Dino: 
+    def __init__(self, un_nombre="", un_color="verde"):
+        self.nombre = un_nombre                 #self es el objeto: pepito
         self.color = un_color
         print("Hola soy un dinosaurio, me llamo", self.nombre, "y soy de color", self.color)
 
-pepito = Dino("pepe", "verde")
-pepita = Dino("pepa", "azul")
+    def cambiar_color(self, un_color):          #metodo es cambiar_color
+        self.color = un_color 
+
+
+
+pepito = Dino("pepe", "verde")   #Instanciamos o creamos el objeto de tipo DinoAtributos de pepito
+print(pepito.nombre)        #Accedemos a la propiedad/Atributo del objeto e imprimimos
+pepito.cambiar_color("Lila")         
+#pepita = Dino("pepa", "azul")
 
 #agregarle una propiedad color a la clase Dino, y que salude
 #diciendo su nombre y de que color es el dinosaurio
